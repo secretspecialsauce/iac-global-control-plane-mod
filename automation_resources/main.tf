141b4b63f0c1aefbd0fdddd9b1d87d8e4743718b
Paul Durivage <1240740+angstwad@users.noreply.github.com>
2022-12-14 16:08:21 -0700

    add storage admin and sort list

--- automation_resources/main.tf
+++ automation_resources/main.tf
@@ -11,28 +11,29 @@ locals {
   project_prefix = var.project_prefix == "" ? "" : "${var.project_prefix}-"
   gcb-tf-iam-roles = [
     "roles/accesscontextmanager.policyAdmin",
-    "roles/billing.user",
     "roles/billing.projectManager",
+    "roles/billing.user",
+    "roles/cloudasset.viewer",
     "roles/compute.networkAdmin",
     "roles/compute.xpnAdmin",
     "roles/iam.securityAdmin",
     "roles/iam.serviceAccountAdmin",
     "roles/logging.configWriter",
+    "roles/logging.logWriter",
     "roles/orgpolicy.policyAdmin",
+    "roles/resourcemanager.folderAdmin",
+    "roles/resourcemanager.organizationAdmin",
+    "roles/resourcemanager.organizationViewer",
+    "roles/resourcemanager.projectCreator",
     "roles/resourcemanager.projectCreator",
     "roles/resourcemanager.projectDeleter",
-    "roles/resourcemanager.folderAdmin",
-    "roles/securitycenter.notificationConfigEditor",
-    "roles/resourcemanager.organizationViewer",
     "roles/resourcemanager.projectIamAdmin",
-    "roles/cloudasset.viewer",
-    "roles/securitycenter.admin",
-    "roles/resourcemanager.organizationAdmin",
     "roles/resourcemanager.tagAdmin",
     "roles/resourcemanager.tagUser",
-    "roles/resourcemanager.projectCreator",
-    "roles/logging.logWriter",
-    "roles/serviceusage.serviceUsageAdmin"
+    "roles/securitycenter.admin",
+    "roles/securitycenter.notificationConfigEditor",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/storage.admin"
   ]
 }
 
